8cf5de3d5371a503a56ae3e81897dccd85dad480
wissal <wissalalhj@gmail.com>
2021-11-29 22:27:44 +0200

    step2-basic3

---
+++ javascript-basics-03/step 2/step2.html+step2.js
@@ -0,0 +1,7 @@
+function f(nb) {
+    if (nb === 2) {
+        document.getElementById('texte').style.display = "none";
+    } else {
+        document.getElementById('texte').style.display = "block";
+    }
+}
