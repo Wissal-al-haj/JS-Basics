5b158a3b75bb710b7e686804f77dbd5f5b13619d
wissal <wissalalhj@gmail.com>
2021-11-29 22:30:52 +0200

    step2-basic3

---
+++ javascript-basics-03/step 3/step3.html+step3.js
@@ -0,0 +1,11 @@
+function f(nb){
+    if (nb===1){
+        document.getElementById('text').style.color="green";
+    }
+    if (nb===2){
+        document.getElementById('text').style.color="red";
+    }
+    if (nb===3){
+        document.getElementById('text').style.color="blue";
+    }
+}
